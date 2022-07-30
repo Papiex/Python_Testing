e5da6c0ccb7e80547dd6926a53b85794c44151d9
Papiexx <boutdepapier27@outlook.fr>
2022-07-30 08:30:56 +0200

    add deduct comp and club tests

--- tests/units/test_clubs.py
+++ tests/units/test_clubs.py
@@ -1,3 +1,6 @@
+from urllib import response
+
+
 def test_should_purchase_place(client):
     """Test buying basic reservations"""
     response = client.post('/purchasePlaces', data = {
@@ -29,3 +32,32 @@ def test_should_not_enought_competition_points(client):
     })
     assert response.status_code == 200
     assert b'This competition does not have enough places' in response.data
+
+
+def test_should_deduct_competition_points(client):
+    """Test for deduct 2 points of Test Competition"""
+    data = {
+      "club": "Simply Lift",
+      "competition": "Test Competition",
+      "places": 2
+    }
+
+    response = client.post('/purchasePlaces', data=data)
+    assert response.status_code == 200
+    assert b'''Number of Places: 6\n            \n                
+    <a href="/book/Test%20Competition/Simply%20Lift">' in response.data'''
+
+
+def test_should_deduct_club_points(client, capsys):
+    """Test for deduct 2 points of Simply Lift club """
+    data = {
+      "club": "Simply Lift",
+      "competition": "Test Competition",
+      "places": 2
+    }
+
+    response = client.post('/purchasePlaces', data=data)
+    with capsys.disabled():
+        print(response.data)
+    assert response.status_code == 200
+    assert b'Points available: 11' in response.data
